74dc1f3b3ecf253fc78618c1b4dab937ca2f3f8f
Guilherme Tavares <guilhermeditavares@gmail.com>
2021-12-13 11:51:43 +0000

    finished experiments.py

--- src/experiments.py
+++ src/experiments.py
@@ -1,4 +1,5 @@
 import math
+import os
 import random
 import subprocess
 
@@ -51,10 +52,26 @@ def convert_tables():
                             fp.write(line + "\n")
 
 
+
 if __name__ == '__main__':
-    random.seed(4361776588)
-    for i in range(10,60,10):
-        for j in (0.05, 0.1, 0.2, 0.25, 0.33):
-            gen_inputs(i,j,math.ceil(random.random()+1000),"../data/input/"+i+'-'+j+".in")
-            subprocess.run(["ls", "-l", "/dev/null"], capture_output=True)
+    with open("seeds.out", "a") as f:
+        random.seed(4361776588)
+        for i in range(10,60,10):
+            for j in (0.05, 0.1, 0.2, 0.25, 0.33):
+                seeded_number = int(str(random.random())[2:])
+                f.write("dataset "+str(i)+" "+str(j)+" "+str(seeded_number)+'\n')
+                gen_inputs(i, j, seeded_number, "../data/input/" + str(i) + '-' + str(j)[2:] + ".in")
+                os.system("echo \"probability: {}\" >> ../data/code1/size_{}.out".format(j,i))
+                os.system("echo \"probability: {}\" >> ../data/code2/size_{}.out".format(j,i))
+                for k in range(1,6,1):
+                    f1 = open("../data/code1/size_{}.out".format(i),"a")
+                    f2 = open("../data/code2/size_{}.out".format(i),"a")
+                    seeded_number = int(str(random.random())[2:])
+                    f.write("experiment code1 " + str(i) + " " + str(j) + " " + str(seeded_number)+'\n')
+                    subprocess.run(["../bin/code1",str(seeded_number), str(1200), "../data/input/{}-{}.in".format(i,str(j)[2:])],stdout=f1)
+                    seeded_number = int(str(random.random())[2:])
+                    f.write("experiment code2 " + str(i) + " " + str(j) + " " + str(seeded_number)+'\n')
+                    subprocess.run(
+                        ["../bin/code2", str(seeded_number), str(1200), "../data/input/{}-{}.in".format(i, str(j)[2:])],
+                        stdout=f2)
     convert_tables()
